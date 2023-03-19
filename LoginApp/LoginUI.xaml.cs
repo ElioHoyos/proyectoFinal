53a4642831f70b01e0d4b7e056c11e63ea515a3a
Zeid franco <zeidfranco@gmail.com>
2023-03-19 17:52:41 -0500

    actualizacion
    desarrollo de mi login

---
+++ LoginApp/LoginUI.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LoginApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginUI : ContentPage
+    {
+        public LoginUI()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if(txtUsuario.Text=="admin" && txtContraseña.Text =="admin")
+            {
+                Navigation.PushAsync(new MainPage());
+            }
+            else
+            {
+                DisplayAlert("","El Usuario o Contraseña es incorrecta","Ok");
+            }
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RegisterPage());
+        }
+    }
+}
---
+++ LoginApp/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.WindowsSpecific;
+
+namespace LoginApp
+{
+    public partial class MainPage : Xamarin.Forms.FlyoutPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            flyout.listView.ItemSelected += OnSelectedItem;
+        }
+
+        private void OnSelectedItem(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as FlyoutItemPage;
+            if (item != null)
+            {
+                Detail = new NavigationPage((Xamarin.Forms.Page)Activator.CreateInstance(item.TargetPage));
+                flyout.listView.SelectedItem = null;
+                IsPresented = false;
+            }
+        }
+    }
+}
