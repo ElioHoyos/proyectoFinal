033f2b612963cae2b2c77716cea1807fc246eea2
Betty FH <117499993+bemi7@users.noreply.github.com>
2023-03-18 21:50:49 -0500

    Trabajo Movil
    Movil

---
+++ ProyectoFinal/ProyectoFinal/Login.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProyectoFinal
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Login : ContentPage
+	{
+		public Login ()
+		{
+            InitializeComponent();
+            txtNombre.Text = Preferences.Get("Nombre Guardado", "");
+            RememberSwitch.IsToggled = Preferences.Get("Guardado Correctamente", false);
+        }
+
+        public async void Button_Clicked(object sender, EventArgs e)
+        {
+
+            string nombre = txtNombre.Text;
+            string correo = txtCorreo.Text;
+            await Application.Current.MainPage.Navigation.PushModalAsync(new SegundaPage(nombre, correo));
+
+        }
+
+        private void RememberMe_toggle_Switch(object sender, EventArgs e)
+        {
+            if (RememberSwitch.IsToggled == true)
+            {
+                Preferences.Set("Nombre Guardado", txtNombre.Text);
+                Preferences.Set("Guardado Correctamente", RememberSwitch.IsToggled == true);
+                RememberSwitch.ThumbColor = Color.Red;
+                RememberSwitch.OnColor = Color.LightCoral;
+            }
+            else
+            {
+                Preferences.Remove("Eliminado Correctamente");
+                Preferences.Remove("Borrar");
+                RememberSwitch.ThumbColor = Color.Gray;
+            }
+
+
+        }
+
+    }
+}
---
+++ ProyectoFinal/ProyectoFinal/SegundaPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProyectoFinal
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SegundaPage : ContentPage
+	{
+        public SegundaPage(string nombre, string correo)
+        {
+            InitializeComponent();
+            LabelNombre.Text = "Un gusto : " + nombre + " " + correo;
+
+
+            LabelCorreo.Text = "Tu correo es : " + correo;
+
+        }
+
+        public async void Button1_Clicked(object sender, EventArgs e)
+        {
+
+            await Application.Current.MainPage.Navigation.PushModalAsync(new MainPage());
+        }
+    }
+}
