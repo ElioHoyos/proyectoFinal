26759af214f2904e408915353b14f24b173d7354
Elio Hoyos <elio18david@gmail.com>
2023-03-20 09:32:35 -0500

    avance proyecto

--- ProyectoFinal/ProyectoFinal/obj/Debug/netstandard2.0/CategoriaPage.xaml.g.cs
+++ ProyectoFinal/ProyectoFinal/obj/Debug/netstandard2.0/CategoriaPage.xaml.g.cs
@@ -17,8 +17,12 @@ namespace ProyectoFinal {
     public partial class CategoriaPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Button btnCategoria;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(CategoriaPage));
+            btnCategoria = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "btnCategoria");
         }
     }
 }
